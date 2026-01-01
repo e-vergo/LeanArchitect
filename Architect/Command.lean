b9e10b2a696038db741936af80fe837cb1ea02fb
Thomas Zhu <thomas.zhu.sh@gmail.com>
2026-01-01 02:21:26 -0800

    do not trim blueprint_command

--- Architect/Command.lean
+++ Architect/Command.lean
@@ -39,7 +39,7 @@ elab (name := blueprintComment) "blueprint_comment " stx:plainDocComment : comma
   let some range ← Elab.getDeclarationRange? stx
     | return  -- must be from partial syntax, ignore
 
-  let doc := (← getDocStringText ⟨stx⟩).trimAscii.copy
+  let doc ← getDocStringText ⟨stx⟩
   modifyEnv fun env => addMainModuleBlueprintDoc env ⟨doc, range⟩
 
 end Architect
