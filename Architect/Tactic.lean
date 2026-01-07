332d765d78d32a031b8b06832b0581816494ddfd
Thomas Zhu <thomas.zhu.sh@gmail.com>
2026-01-06 17:19:01 -0800

    chore: change proofDocString to persistent and add lake tests

--- Architect/Tactic.lean
+++ Architect/Tactic.lean
@@ -11,20 +11,30 @@ namespace ProofDocString
 
 /-! Here we implement docstrings but for proofs. -/
 
-/-- The environment extension that stores the proof docstrings.
-Note that this is not persistent, so the data is not available to other modules. -/
-initialize proofDocStringExt : EnvExtension (NameMap (Array String)) ←
-  registerEnvExtension (pure ∅) (asyncMode := .async .asyncEnv)
+-- NB: I copied some logic from `aliasExtension`
+
+abbrev State := SMap Name (Array String)
+abbrev Entry := Name × String
+
+private def addEntryFn (s : State) (e : Entry) : State :=
+  match s.find? e.1 with
+  | none => s.insert e.1 #[e.2]
+  | some es => s.insert e.1 (es.push e.2)
+
+/-- The environment extension that stores proof docstrings. -/
+initialize proofDocStringExt : SimplePersistentEnvExtension Entry State ←
+  registerSimplePersistentEnvExtension {
+    addEntryFn := addEntryFn
+    addImportedFn := fun es => mkStateFromImportedEntries addEntryFn {} es |>.switch
+    asyncMode := .async .asyncEnv
+  }
 
 end ProofDocString
 
 open ProofDocString
 
 def addProofDocString (env : Environment) (name : Name) (doc : String) : Environment :=
-  proofDocStringExt.modifyState (asyncDecl := name) env fun s =>
-    match s.find? name with
-    | none => s.insert name #[doc]
-    | some es => s.insert name (es.push doc)
+  proofDocStringExt.addEntry (asyncDecl := name) env (name, doc)
 
 def getProofDocString (env : Environment) (name : Name) : String :=
   "\n\n".intercalate <|
---
+++ ArchitectTest/AsyncElab.lean
@@ -0,0 +1,32 @@
+import Architect
+
+open Lean Elab Term Std.Time in
+elab "now_ms%" : term => do
+  -- Synchronize env
+  let env := (← getEnv).checked.get
+  let hasC := env.constants.contains `c
+  logInfo m!"Contains `c`: {hasC}"
+  -- Get time
+  let now ← Timestamp.now
+  let ms := now.toMillisecondsSinceUnixEpoch.toInt.natAbs
+  elabTerm (Syntax.mkNatLit ms) (mkConst ``Nat)
+
+/-- info: Contains `c`: false -/
+#guard_msgs in
+def old := now_ms%
+
+theorem a : True := by /-- a -/ sleep 1000; trivial
+theorem b : True := by /-- b -/ sleep 1000; trivial
+theorem c : True := by /-- c -/ sleep 1000; trivial
+
+/-- info: Contains `c`: true -/
+#guard_msgs in
+def new := now_ms%
+
+open Lean Architect in
+run_meta
+  -- Running a, b, c should be all parallel and hence the time should be ≈1s and ≪2s
+  assert! new - old < 1100
+  assert! getProofDocString (← getEnv) ``a == "a"
+  assert! getProofDocString (← getEnv) ``b == "b"
+  assert! getProofDocString (← getEnv) ``c == "c"
